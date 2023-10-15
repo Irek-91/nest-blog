f7642bfd1868243d53b0322d122f62f3c785263b
Irek Shamilov <shamilov.irek@bk.ru>
2023-10-15 22:55:05 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,13 @@ import { BlogsController } from './blogs/blogs.controller';
 import { BlogsService } from './blogs/blogs.service';
 import { BlogsRepository } from './blogs/blogs.repo';
 import { Blog, BlogSchema } from './blogs/models/blogs-schema';
+import * as process from 'process';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb+srv://admin:admin1@atlascluster.0x495z3.mongodb.net/BlogPlatform?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(process.env.MONGO_URL!),
+      //'mongodb+srv://admin:admin1@atlascluster.0x495z3.mongodb.net/BlogPlatform?retryWrites=true&w=majority'),
     //MongooseModule.forRoot('mongodb://localhost:27017/nest_blog'),
     MongooseModule.forFeature([
       {
