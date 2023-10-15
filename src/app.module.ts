395ec1bf86bfca05d5ef3768deeb1b106e7d798c
Irek Shamilov <shamilov.irek@bk.ru>
2023-10-15 22:23:54 +0300

    fix vercel and DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import { ConfigModule } from '@nestjs/config';
+
 import { Module } from '@nestjs/common';
 import { AppController, TestingController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,11 +12,10 @@ import { BlogsController } from './blogs/blogs.controller';
 import { BlogsService } from './blogs/blogs.service';
 import { BlogsRepository } from './blogs/blogs.repo';
 import { Blog, BlogSchema } from './blogs/models/blogs-schema';
-import { ConfigModule } from 'nestjs-config';
 
 @Module({
   imports: [
-    //ConfigModule.forRoot(),
+    ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb+srv://admin:admin1@atlascluster.0x495z3.mongodb.net/BlogPlatform?retryWrites=true&w=majority'),
     //MongooseModule.forRoot('mongodb://localhost:27017/nest_blog'),
     MongooseModule.forFeature([
