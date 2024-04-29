f952ce262f3a5d248e15bd040f598926db5776f5
Irek Shamilov <shamilov.irek@bk.ru>
2024-04-29 10:40:39 +0300

    fix:app.service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Hello Irek';
+    return "Hey buddy. If you can see that, it's probably working.";
   }
 }
